b773c4f7801c7b940d8db0867848f041e6b27745
MKody <im@kdy.ch>
2019-04-11 12:19:57 +0000

    Add filters to some tags

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+/* Look if we provide the filename */
 $filename = ""; // Preventing notices
 if(isset($_GET['f'])) {
 	$filename = substr($_GET['f'], 1); // Get file name
@@ -6,10 +7,53 @@ if(isset($_GET['f'])) {
 	if(substr($filename, -3) == ".md") $filename = substr($filename, 0, -3); // Remove extension
 }
 
+/* Init */
 require('parsedown/Parsedown.php'); // Parsedown lib
 require('parsedown-extra/ParsedownExtra.php'); // ParsedownExtra lib
-$Extra = new ParsedownExtra(); // Create object
+require('ParsedownFilter/ParsedownFilter.php'); // ParsedownFilter lib
+$parser = new ParsedownFilter('ourFilters');
 
+/* Settings */
+$parser->setBreaksEnabled(true);
+
+function ourFilters(&$el) {
+	switch($el['name']){
+		case 'a':
+			// Add rel and target to external links
+			$url = $el['attributes']['href'];
+			if(strpos($url, '://') === false) { // If it doesn't have a scheme...
+				// Let's make sure it's not a "//" shortcut
+				if((($url[0] == '/') && ($url[1] != '/')) || ($url[0] != '/')) {
+					return;
+				}
+			}
+                        // If the link doesn't share the host, then it's external
+			if(strpos($url, $_SERVER["SERVER_NAME"]) === false) {
+				$el['attributes']['rel'] = 'noopener nofollow';
+				$el['attributes']['target'] = '_blank';
+			}
+			break;
+		case 'img':
+			// Add lazy loading attribute to images
+			$el['attributes']['loading'] = 'lazy';
+			break;
+		case 'h1':
+		case 'h2':
+		case 'h3':
+		case 'h4':
+		case 'h5':
+		case 'h6':
+			// Create a slug and set as id of headings
+                        $pattern = '/[^a-z0-9àäéèêôõùüû\\-]/';
+                        $slug = strtolower($el['handler']['argument']);
+                        $slug = str_replace(' ', '-', $slug);
+                        $slug = preg_replace($pattern, '', $slug);
+			$el['attributes']['id'] = $slug;
+			break;
+	}
+}
+
+/* Try to load document */
 $dir = "data/"; // Where are the .md stored (can be relative or absolue path)
 $markdown = @file_get_contents($dir . $filename . '.md'); // Read the file
 $canDL = false; // Preventing notices messages from PHP
@@ -37,7 +81,8 @@ There's nothing here...";
 <title>Notes <?php if(!empty($filename)) echo " > ". $filename; ?></title>
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 <style>
-	body {margin: 0 15px;}
+	body {margin: 0 15px 50px;}
+	td, th {padding: 0 5px;}
 	/* I don't like when images are too high */
 	img {max-width: 100%; max-height: 400px; height: auto;}
 	.dl-md {position: absolute; right: 5px; top: 0;}
@@ -46,10 +91,10 @@ There's nothing here...";
 <body>
 <?php
 // If the file exist, make it downloadable
-if($canDL) echo '<a class="dl-md" href="/data/'. $filename .'.md">Download Markdown</a>';
+if($canDL) echo '<a class="dl-md" href="/data/'. $filename .'.md">Download .md</a>';
 
 // Render Markdown, new lines are converted to <br>
-echo $Extra->setBreaksEnabled(true)->text($markdown);
+echo $parser->text($markdown);
 ?>
 
 <!-- Using MKody's spaghetti code
